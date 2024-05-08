37ddd3f614a01ddd91fae83bd93bfe8015c6bccf
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-05-09 01:07:57 +0300

    Started work on second iteration

--- ChatBot/basic.py
+++ ChatBot/basic.py
@@ -2,13 +2,13 @@ import json
 from difflib import get_close_matches
 
 
-def load_knoladege_base(file_path: str) -> dict:
+def load_base(file_path: str) -> dict:
     with open(file_path, "r") as file:
         data: dict = json.load(file)
     return data
 
 
-def save_knowledge_base(file_path: str, data: dict):
+def save_base(file_path: str, data: dict):
     with open(file_path, "w") as file:
         json.dump(data, file, indent=2)
 
@@ -25,8 +25,22 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
             return q["answer"]
 
 
+def add_answer_from_user(path_to_knowledge: str, knowledge_base: dict, user_input: str) -> None: # noqa
+    print('Bot: I don\'t know the answer, Can you teach me?\n')
+    new_answer: str = input('Type the answer or "skip" to skip:\n') # noqa
+
+    if new_answer.lower() != "skip":
+        knowledge_base["questions"].append({"question": user_input, "answer": new_answer}) # noqa
+        save_base(path_to_knowledge, knowledge_base)
+        print('Bot: Thank you! I learned a new response!')
+
+
+def add_question_to_unanswered(path_to_unanswered: str, user_input: str) -> None: # noqa
+    raise NotImplementedError
+
+
 def chat_bot(path_to_knowledge: str):
-    knowledge_base: dict = load_knoladege_base(path_to_knowledge)
+    knowledge_base: dict = load_base(path_to_knowledge)
 
     while True:
         user_input: str = input("You: ")
@@ -41,15 +55,7 @@ def chat_bot(path_to_knowledge: str):
             answer: str = get_answer_for_question(best_match, knowledge_base)
             print(f'Bot: {answer}')
         else:
-            print('Bot: I don\'t know the answer, Can you teach me?\n')
-            new_answer: str = input('Type the answer or "skip" to skip or "exit" to exit: ') # noqa
-
-            if new_answer.lower() == "exit":
-                break
-            elif new_answer.lower() != "skip":
-                knowledge_base["questions"].append({"question": user_input, "answer": new_answer}) # noqa
-                save_knowledge_base(path_to_knowledge, knowledge_base)
-                print('Bot: Thank you! I learned a new response!')
+            add_answer_from_user(path_to_knowledge, knowledge_base, user_input)
 
 
 def main() -> None:

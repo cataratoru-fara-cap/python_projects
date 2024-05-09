93d090f314fbbb264da1e0a24fbd094697c912a1
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-05-10 00:17:56 +0300

    change req to .yml

--- LLMbot/chatgpt.py
+++ LLMbot/chatgpt.py
@@ -6,7 +6,7 @@ import openai
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
